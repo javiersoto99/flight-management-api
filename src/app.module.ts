470b6a6d1d2d6cbd11a6ee0bce5f5009e408b3ef
javiersoto <javier.soto@i-car.cl>
2025-05-07 18:58:51 -0400

    Docker implementation

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FlightsModule } from './flights/flights.module';
-import { FlightsModule } from './flights/flights.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(
       process.env.MONGODB_URI ||
-        'mongodb://admin:password@localhost:27017/flights?authSource=admin',
+        'mongodb://admin:password@localhost:27017/flights?authSource=admin'
     ),
     FlightsModule,
   ],
