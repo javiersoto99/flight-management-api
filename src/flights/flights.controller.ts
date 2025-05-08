fcdd658eb89924468ac286ee594d9b6a50ef2383
javiersoto <javier.soto@i-car.cl>
2025-05-08 00:52:31 -0400

    Added service and controller logic

--- src/flights/flights.controller.ts
+++ src/flights/flights.controller.ts
@@ -1,7 +1,37 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { FlightsService } from './flights.service';
+import { CreateFlightDto } from './dto/create-flight.dto';
+import { UpdateFlightDto } from './dto/update-flight.dto';
 
 @Controller('flights')
 export class FlightsController {
   constructor(private readonly flightsService: FlightsService) {}
+
+  @Get()
+  findAll() {
+    return this.flightsService.findAll();
+  }
+
+  @Post()
+  create(@Body() createFlightDto: CreateFlightDto) {
+    return this.flightsService.create(createFlightDto);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateFLightDto: UpdateFlightDto) {
+    return this.flightsService.update(id, updateFLightDto);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.flightsService.delete(id);
+  }
 }
--- src/flights/flights.service.ts
+++ src/flights/flights.service.ts
@@ -1,4 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Flight } from './schemas/flight.schema';
+import { Model } from 'mongoose';
+import { CreateFlightDto } from './dto/create-flight.dto';
+import { UpdateFlightDto } from './dto/update-flight.dto';
 
 @Injectable()
-export class FlightsService {}
+export class FlightsService {
+  constructor(@InjectModel(Flight.name) private flightModel: Model<Flight>) {}
+
+  async create(createFlightDto: CreateFlightDto): Promise<Flight> {
+    const newFlight: Flight = new this.flightModel(createFlightDto);
+
+    return newFlight.save();
+  }
+
+  async update(id: string, updateFlightDto: UpdateFlightDto): Promise<Flight> {
+    const updatedFlight = await this.flightModel
+      .findByIdAndUpdate(id, updateFlightDto, { new: true })
+      .exec();
+
+    if (!updatedFlight) {
+      throw new NotFoundException('Flight not found');
+    }
+
+    return updatedFlight;
+  }
+
+  async delete(id: string): Promise<Flight> {
+    const flightToDelete = await this.flightModel.findByIdAndDelete(id).exec();
+
+    if (!flightToDelete) {
+      throw new NotFoundException('Flight not found');
+    }
+
+    return flightToDelete;
+  }
+
+  async findAll(): Promise<Flight[]> {
+    const flights: Flight[] = await this.flightModel.find().exec();
+
+    return flights;
+  }
+}
